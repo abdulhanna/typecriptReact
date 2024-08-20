0b9110acbdd1c5844d94a4188e7c7dfbf3f8297c
Abdul Hannann <abdul.hannan@plaxonic.com>
2024-08-20 23:52:02 +0530

    type assetion

--- src/components/state/user.tsx
+++ src/components/state/user.tsx
@@ -6,7 +6,8 @@ type AuthUser = {
 }
 
 export const User = ()=>{
-    const [user,setUser] = useState<AuthUser| null>(null)
+    // const [user,setUser] = useState<AuthUser| null>(null)
+    const [user,setUser] = useState<AuthUser>({} as AuthUser)  // Assetion type
     
     const handleLogin = ()=>{
         setUser({
@@ -14,14 +15,14 @@ export const User = ()=>{
             email:"abdul@gmail.com"
         })
     }
-    const handleLogout = ()=>{
-        setUser(null)
-    }
+    // const handleLogout = ()=>{
+    //     setUser(null)
+    // }
     return <>
     <button onClick={handleLogin}>Login</button>
-    <button onClick={handleLogout}>Logout</button>
-    <div>User Name is ${user?.name}</div>
-    <div>User Email is ${user?.email}</div>
+    {/* <button onClick={handleLogout}>Logout</button> */}
+    <div>User Name is ${user.name}</div>
+    <div>User Email is ${user.email}</div>
     </>
 
 }
